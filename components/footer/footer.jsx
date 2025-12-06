5195b5d629070c04f79e585359e3d4ab0d83ce61
Cauã <cauadeassis2009@gmail.com>
2025-12-06 20:00:52 +0000

    Add footer instagram links

--- components/footer/footer.jsx
+++ components/footer/footer.jsx
@@ -7,12 +7,12 @@ export default function Footer() {
             <p>Direitos reservados © 2025</p>
             <h2 className={sharedStyles.mainTitle}>Membros</h2>
             <ul className={sharedStyles.list}>
-                <a href="#" >Caleb Jorge</a>
-                <a href="#" >Cauã de Assis</a>
+                <a href="https://www.instagram.com/caleb_joorg/" >Caleb Jorge</a>
+                <a href="https://www.instagram.com/sk8_cauaa/" >Cauã de Assis</a>
                 <a href="https://www.instagram.com/vttr_jv/" >João Vitor</a>
-                <a href="#" >Paulo César</a>
-                <a href="#" >Samuel Soares</a>
-                <a href="#" >Thiago Cascelli</a>
+                <a href="https://www.instagram.com/pcleitao/" >Paulo César</a>
+                <a href="https://www.instagram.com/samuelsoares1346/" >Samuel Soares</a>
+                <a href="https://www.instagram.com/thiago_acn/" >Thiago Cascelli</a>
             </ul>
         </footer>
     );
