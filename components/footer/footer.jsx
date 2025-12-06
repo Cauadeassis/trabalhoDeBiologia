3f3f04c539b897a7300724ab104e6849961ae878
Cauã <cauadeassis2009@gmail.com>
2025-12-06 19:53:24 +0000

    Simplification of `components`

--- components/footer/footer.jsx
+++ components/footer/footer.jsx
@@ -1,22 +1,19 @@
 import React from "react";
-import styles from "./footer.module.css";
-import sharedStyles from "../../styles/shared.module.css";
+import sharedStyles from "../shared.module.css";
 
 export default function Footer() {
     return (
-        <footer className={styles.footer}>
-            <div>
-                <p>Direitos reservados © 2025</p>
-                <h2>Membros</h2>
-                <ul>
-                    <a href="#" className={sharedStyles.glassLink}>Caleb Jorge</a>
-                    <a href="#" className={sharedStyles.glassLink}>Cauã de Assis</a>
-                    <a href="#" className={sharedStyles.glassLink}>João Vitor</a>
-                    <a href="#" className={sharedStyles.glassLink}>Paulo César</a>
-                    <a href="#" className={sharedStyles.glassLink}>Samuel Soares</a>
-                    <a href="#" className={sharedStyles.glassLink}>Thiago Cascelli</a>
-                </ul>
-            </div>
+        <footer className={sharedStyles.modernLayout}>
+            <p>Direitos reservados © 2025</p>
+            <h2 className={sharedStyles.mainTitle}>Membros</h2>
+            <ul className={sharedStyles.list}>
+                <a href="#" >Caleb Jorge</a>
+                <a href="#" >Cauã de Assis</a>
+                <a href="https://www.instagram.com/vttr_jv/" >João Vitor</a>
+                <a href="#" >Paulo César</a>
+                <a href="#" >Samuel Soares</a>
+                <a href="#" >Thiago Cascelli</a>
+            </ul>
         </footer>
     );
 }
--- components/header/header.jsx
+++ components/header/header.jsx
@@ -1,17 +1,16 @@
 import React from "react";
-import styles from "./header.module.css";
-import sharedStyles from "../../styles/shared.module.css";
+import sharedStyles from "../shared.module.css";
 
 export default function Header() {
     return (
-        <div className={styles.header}>
-            <h1>Tecido Epitelial</h1>
-            <nav className={styles.nav}>
-                <a href="#introduction" className={sharedStyles.glassLink}>Introdução</a>
-                <a href="#types" className={sharedStyles.glassLink}>Tipos</a>
-                <a href="#functions" className={sharedStyles.glassLink}>Funções</a>
-                <a href="#glandular" className={sharedStyles.glassLink}>Glandular</a>
-                <a href="#quiz" className={sharedStyles.glassLink}>Quiz</a>
+        <div className={sharedStyles.modernLayout}>
+            <h1 className={sharedStyles.mainTitle}>Tecido Epitelial</h1>
+            <nav className={sharedStyles.list}>
+                <a href="#introduction" >Introdução</a>
+                <a href="#types" >Tipos</a>
+                <a href="#functions"> Funções</a>
+                <a href="#glandular" >Glandular</a>
+                <a href="#quiz" >Quiz</a>
             </nav>
         </div>
     );
